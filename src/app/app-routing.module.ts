be0c68fe0f80da1d7368e2994ff8acd233ecb206
charly <jauffrit.charly@hotmail.com>
2023-02-17 12:33:48 +0100

    queteRouter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'signup' , component: SignUpComponent},
+  {path: 'user' , component: UserProfilComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignUpComponent,
+    UserProfilComponent
   ],
   imports: [
     BrowserModule,
